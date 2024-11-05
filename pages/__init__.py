ae15fbb255806efa5316509a97d0572ac6c64f23
martinlinchr <124469673+martinlinchr@users.noreply.github.com>
2024-11-05 14:15:55 +0100

    Update __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,9 +1,5 @@
-"""
-Page components for the Persona Creator & Chat Application
-"""
-
-from app.pages.create_persona import render_create_persona_page
-from app.pages.chat_with_persona import render_chat_page
+from .create_persona import render_create_persona_page
+from .chat_with_persona import render_chat_page
 
 __all__ = [
     'render_create_persona_page',
