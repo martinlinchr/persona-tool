c3c8cf6c5217800b83aeb05d631ecdf41156a758
martinlinchr <124469673+martinlinchr@users.noreply.github.com>
2024-11-05 13:49:42 +0100

    Update __init__.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,1 +1,14 @@
+"""
+Utility functions for the Persona Creator & Chat Application
+"""
 
+from app.utils.openai_helpers import initialize_openai_client, handle_openai_error
+from app.utils.file_handlers import validate_file, format_file_size, get_file_icon
+
+__all__ = [
+    'initialize_openai_client',
+    'handle_openai_error',
+    'validate_file',
+    'format_file_size',
+    'get_file_icon'
+]
